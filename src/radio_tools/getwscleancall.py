3cabf587405b7f63fb328480ea5cce3b703ae147
Christian Groeneveld <groeneveld@student18.strw.leidenuniv.nl>
2023-07-27 13:58:41 +0200

    Added getwscleancall

---
+++ src/radio_tools/getwscleancall.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3.9
+
+from astropy.io import fits
+import sys
+
+def main():
+    filename = sys.argv[1]
+
+    head = fits.getheader(filename)
+    hist = head['HISTORY']
+    call = ''.join(hist)
+    return call
